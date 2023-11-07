9951437cd4b613fbfa5417f14a94c650e811783a
NokaK <nokakipiani93@gmail.com>
2023-11-07 22:02:16 +0400

    add script file

---
+++ script.js
@@ -0,0 +1,41 @@
+var userName = "John";
+
+userName = "Jane"; //string
+// console.log(userName);
+
+let age = 30; //number
+age = 31;
+
+const birthYear = 1991;
+
+let isMarried = false; //boolean
+
+// console.log(typeof test);
+
+// let person = null;
+// console.log(person);
+let person = {
+  name: "John",
+  age: 30,
+  isMarried: false, //object
+};
+console.log(
+  " My name is" +
+    " " +
+    person.name +
+    " " +
+    "and I am" +
+    " " +
+    person.age +
+    " " +
+    "years old"
+);
+console.log(`My name is ${person.name} and I am ${person.age} years old`);
+
+var x = 10;
+var y = 10;
+var z = 5;
+
+var sum = y + z + x;
+
+console.log(sum);
